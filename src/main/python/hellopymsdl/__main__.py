b5ecd64b15e80ef06ecc4c74b6b999f744ff12ef
St4rG00se <st4rg00se@protonmail.com>
2022-04-26 17:50:56 +0200

    feat: update dependencies and fix new errors

--- src/main/python/hellopymsdl/__main__.py
+++ src/main/python/hellopymsdl/__main__.py
@@ -3,7 +3,7 @@ from hellopymsdl.service.MessageService import MessageService
 
 def hello() -> None:
     print("hello python with Maven Standard Directory Layout")
-    message_service: MessageService = MessageService()
+    message_service: MessageService = MessageService()  # pragma: no mutate
     print(message_service.get_message("message.txt"))
 
 
